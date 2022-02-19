7eec80da6ffc5e64643e475a7d2782597b854ccc
mdayubali <ayubh9288@gmail.com>
2022-02-19 17:54:55 +0600

    new code added

---
+++ js/custom.js
@@ -0,0 +1,117 @@
+
+/*===========Table of the Content==========
+    1.scrool to top
+    2.contdown
+    3.typewrite js
+    4.Wow js
+
+ =============================================*/
+
+   $(document).ready(function(){
+    "use strict";
+    /*=======Navbar Add remove Class===========*/
+    $("nav ul li").click(function(){
+           $("nav ul li").removeClass('current-menu-item');
+            $(this).addClass('current-menu-item');
+      });
+
+    /*============scroll to top ===============*/
+
+    if ($('#back-to-top').length) {
+    var scrollTrigger = 100, // px
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('#back-to-top').addClass('show');
+            } else {
+                $('#back-to-top').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+        backToTop();
+    });
+    $('#back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 800);
+    });
+}
+
+/*==============wow.js===========*/
+    new WOW().init();
+}) 
+   
+//contdown
+
+window.onload=function(){
+    $('.circleGraphic').circleGraphic({});
+    $('.circleGraphic2').circleGraphic({});
+    $('.circleGraphic3').circleGraphic({});
+    $('.circleGraphic4').circleGraphic({});
+    $('.circleGraphic5').circleGraphic({});
+    $('.circleGraphic6').circleGraphic({});
+
+    var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+}
+
+
+/*====================typewrite js======================*/
+
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+
+
+    
